54aafe60d3f6b8be633bbc63364ee3d51d94a0b2
Joseph Chavez <rommelchavez99@gmail.com>
2020-01-12 16:54:19 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,9 @@
 # Build your say_hello method here
+def say_hello(name)
+  if name= "Hi my name is Joseph"
+    puts "Hello Joseph!!"
+  else 
+    puts "Hello Ruby Progammer!"
+  end
+end
+say_hello("Joseph")
